5ba65e7271429188e852fd67efb6f7836387e0be
Jakooob14 <sokol.3@seznam.cz>
2022-09-11 11:10:41 +0200

    Fix 'Empty components are self-closing'

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,9 @@ import emailIcon from '../public/email.svg'
 const Header: NextPage = () => {
     return (
         <footer className={footer.main}>
-            <a href='https://github.com/Jakooob14' target='_BLANK' rel="noreferrer" className={`${footer.icon} ${footer.github}`}><div><Image src={githubLogo} alt="Github logo"></Image></div></a>
-            <a href='https://instagram.com/Jakooob14' target='_BLANK' rel="noreferrer" className={`${footer.icon} ${footer.instagram}`}><div><Image src={instagramLogo} alt="Instagram logo"></Image></div></a>
-            <a href='mailto:sokoljakub14@gmail.com' className={`${footer.icon} ${footer.email}`}><div><Image src={emailIcon} alt="Email icon"></Image></div></a>
+            <a href='https://github.com/Jakooob14' target='_BLANK' rel="noreferrer" className={`${footer.icon} ${footer.github}`}><div><Image src={githubLogo} alt="Github logo"/></div></a>
+            <a href='https://instagram.com/Jakooob14' target='_BLANK' rel="noreferrer" className={`${footer.icon} ${footer.instagram}`}><div><Image src={instagramLogo} alt="Instagram logo"/></div></a>
+            <a href='mailto:sokoljakub14@gmail.com' className={`${footer.icon} ${footer.email}`}><div><Image src={emailIcon} alt="Email icon"/></div></a>
         </footer>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,15 +18,15 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <main className={styles.main}>
-        <div className={styles.loadingoverlay}></div>
+        <div className={styles.loadingoverlay}/>
         <div id='bgwrapper' className={styles.bgwrapper}>
-          <div id='bg' className={`${styles.bgimage} ${styles.bg}`}></div>
-          <div id='stars' className={`${styles.bgimage} ${styles.stars}`}></div>
+          <div id='bg' className={`${styles.bgimage} ${styles.bg}`}/>
+          <div id='stars' className={`${styles.bgimage} ${styles.stars}`}/>
           <h1 id='name'>Jakooob</h1>
           <h2 id='occ'>student & developer</h2>
-          <div id='bgmountains' className={`${styles.bgimage} ${styles.bgmountains}`}><Image src={bgMountains} layout='fill' alt="Background mountains"></Image></div>
-          <div id='gradient' className={`${styles.bgimage} ${styles.gradient}`}><Image src={gradient}layout='fill' alt="Gradient"></Image></div>
-          <div id='fgmountains' className={`${styles.bgimage} ${styles.fgmountains}`}><Image src={fgMountains}layout='fill' alt="Foreground mountains"></Image></div>
+          <div id='bgmountains' className={`${styles.bgimage} ${styles.bgmountains}`}><Image src={bgMountains} layout='fill' alt="Background mountains"/></div>
+          <div id='gradient' className={`${styles.bgimage} ${styles.gradient}`}><Image src={gradient}layout='fill' alt="Gradient"/></div>
+          <div id='fgmountains' className={`${styles.bgimage} ${styles.fgmountains}`}><Image src={fgMountains}layout='fill' alt="Foreground mountains"/></div>
         </div>
         <div className={styles.about}>
           <h2>About me</h2>
