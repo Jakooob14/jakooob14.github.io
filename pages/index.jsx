e0a4a01a794ee4ddd8fe1b6a493a87721bcacc47
Jakooob <sokoljakub14@gmail.com>
2022-10-19 19:25:39 +0200

    Added a logo and put the Head into a component

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,14 +7,13 @@ import gradient from '../public/mountains/gradient.png'
 import Image from 'next/image'
 import Head from 'next/head'
 import Link from 'next/link'
+import DHead from "../components/Head";
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>Home</title>
-         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      <DHead title="Jakooob | Home"/>
+
       <Header />
       <main className={styles.main}>
         <div className={styles.loadingoverlay}/>
@@ -32,7 +31,7 @@ const Home = () => {
           <p>Hello, my name is Jakub Sokol I am an IT student from the Czech Republic and I have always had an adoration for computers and other forms of technology since I was little. My passion only grew stronger as I got to understand the inner workings of games and now my dream is to develop them. </p>
           <span className='_spacer'></span>
           <p>See my projects at the <Link href='/projects'>projects</Link> page. And you can contact me either through my email <a href='mailto:sokoljakub14@gmail.com'>sokoljakub14@gmail.com</a> or Instagram <a href='https://instagram.com/Jakooob14' target='_BLANK' rel="noreferrer noopener">Jakooob14</a>.</p>
-          
+
         </div>
       </main>
       <Footer />
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -5,34 +5,33 @@ import nepIcon from '../public/nep icon.png'
 import mejsczIcon from '../public/mejs.cz icon.svg'
 import Image from 'next/image'
 import Head from 'next/head'
+import DHead from "../components/Head";
 
 const Projects = () => {
   return (
     <>
-    <Head>
-        <title>Projects</title>
-         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
-    <Header />
-    <main className={styles.main}>
-        <div className={styles.projects}>
-            <div className={styles.project}>
-                <span><Image src={nepIcon} layout='fill' objectFit='contain' alt="Not enough PvP image"/></span>
-                <div>
-                    <h1><a href='https://github.com/Jakooob14/Not-Enough-PvP' target='_BLANK' rel='noreferrer noopener'>Not Enough PvP</a> (2022)</h1>
-                    <p>Not Enough PvP or just NEP is a simple mod for Minecraft Fabric 1.19.2 that adds a missing armor HUD. So when you&apos;re in a fight you know instantly when an armor piece breaks so you can put on a new one. If I have more ideas I will add more features. If you have any ideas for this mod you can post them in the GitHub repo <a href='https://github.com/Jakooob14/Not-Enough-PvP/issues' target='_BLANK' rel='noreferrer noopener'>issues tab</a>.</p>
-                </div>
-            </div>
+        <DHead title="Jakooob | Projects"/>
+
+        <Header />
+        <main className={styles.main}>
+            <div className={styles.projects}>
                 <div className={styles.project}>
-                <span><Image src={mejsczIcon} layout='fill' objectFit='contain' alt="Mejs cz image"/></span>
-                <div>
-                    <h1><a href='https://mejs.cz/' target='_BLANK' rel='noreferrer noopener'>Mejs.cz</a> (2022)</h1>
-                    <p>Mejs.cz is a Czech website made with NextJS for a Minecraft server with the same name. I made this website with my friend <a href='https://github.com/Skymmel' target='_BLANK' rel='noreferrer noopener'>Skymmel</a>.</p>
+                    <span><Image src={nepIcon} layout='fill' objectFit='contain' alt="Not enough PvP image"/></span>
+                    <div>
+                        <h1><a href='https://github.com/Jakooob14/Not-Enough-PvP' target='_BLANK' rel='noreferrer noopener'>Not Enough PvP</a> (2022)</h1>
+                        <p>Not Enough PvP or just NEP is a simple mod for Minecraft Fabric 1.19.2 that adds a missing armor HUD. So when you&apos;re in a fight you know instantly when an armor piece breaks so you can put on a new one. If I have more ideas I will add more features. If you have any ideas for this mod you can post them in the GitHub repo <a href='https://github.com/Jakooob14/Not-Enough-PvP/issues' target='_BLANK' rel='noreferrer noopener'>issues tab</a>.</p>
+                    </div>
+                </div>
+                    <div className={styles.project}>
+                    <span><Image src={mejsczIcon} layout='fill' objectFit='contain' alt="Mejs cz image"/></span>
+                    <div>
+                        <h1><a href='https://mejs.cz/' target='_BLANK' rel='noreferrer noopener'>Mejs.cz</a> (2022)</h1>
+                        <p>Mejs.cz is a Czech website made with NextJS for a Minecraft server with the same name. I made this website with my friend <a href='https://github.com/Skymmel' target='_BLANK' rel='noreferrer noopener'>Skymmel</a>.</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </main>
-    <Footer />
+        </main>
+        <Footer />
     </>
   )
 }
