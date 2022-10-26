bcbd26ae1a28cd6f04ad42f2e1db4adb0e0f0106
Jakooob <sokoljakub14@gmail.com>
2022-10-26 22:18:39 +0200

    commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,6 @@ import bgMountains from '../public/mountains/bgmountains.png'
 import fgMountains from '../public/mountains/fgmountains.png'
 import gradient from '../public/mountains/gradient.png'
 import Image from 'next/image'
-import Head from 'next/head'
 import Link from 'next/link'
 import DHead from "../components/Head";
 
@@ -26,13 +25,12 @@ const Home = () => {
           <div id='gradient' className={`${styles.bgimage} ${styles.gradient}`}><Image src={gradient}layout='fill' alt="Gradient" priority={true}/></div>
           <div id='fgmountains' className={`${styles.bgimage} ${styles.fgmountains}`}><Image src={fgMountains}layout='fill' alt="Foreground mountains" priority={true}/></div>
         </div>
-        <div className={styles.about}>
-          <h2>About me</h2>
-          <p>Hello, my name is Jakub Sokol I am an IT student from the Czech Republic and I have always had an adoration for computers and other forms of technology since I was little. My passion only grew stronger as I got to understand the inner workings of games and now my dream is to develop them. </p>
-          <span className='_spacer'></span>
-          <p>See my projects at the <Link href='/projects'>projects</Link> page. And you can contact me either through my email <a href='mailto:sokoljakub14@gmail.com'>sokoljakub14@gmail.com</a> or Instagram <a href='https://instagram.com/Jakooob14' target='_BLANK' rel="noreferrer noopener">Jakooob14</a>.</p>
-
-        </div>
+          <div className={styles.about}>
+              <h2>About me</h2>
+              <p>Hello, my name is Jakub Sokol I am an IT student from the Czech Republic and I have always had an adoration for computers and other forms of technology since I was little. My passion only grew stronger as I got to understand the inner workings of games and now my dream is to develop them. </p>
+              <span className='_spacer'></span>
+              <p>See my projects at the <Link href='/projects'>projects</Link> page. And you can contact me either through my email <a href='mailto:sokoljakub14@gmail.com'>sokoljakub14@gmail.com</a> or Instagram <a href='https://instagram.com/Jakooob14' target='_BLANK' rel="noreferrer noopener">Jakooob14</a>.</p>
+          </div>
       </main>
       <Footer />
     </>
