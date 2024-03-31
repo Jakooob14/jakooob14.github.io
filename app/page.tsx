0813dfff1439ebca3d40ace8384c80672165c732
Jakooob14 <sokoljakub14@gmail.com>
2024-03-31 18:19:15 +0200

    404 page on index

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
+import NotFound from "@/app/not-found";
 
 export default function Page() {
     return (
         <>
-            <h1 className={"text-3xl"}>Seems like the middleware did not work...</h1>
-            <p>If you can please report this at <a href={'mailto:me@jakooob.dev'}>me@jakooob.dev</a></p>
-            <p>Get to the right site <Link href={"/en"}>here</Link></p>
+            <NotFound/>
         </>
     )
 }
