cabec7fe2f232d5e0a0d8d6d3c400b4f1c39457c
Jakooob14 <sokoljakub14@gmail.com>
2024-11-19 23:29:53 +0100

    Main title finally working as it should

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {ReactNode, useEffect} from 'react';
+import {ReactNode, Suspense, useCallback, useEffect, useRef, useState} from 'react';
 import anime from 'animejs/lib/anime.es.js'
 
 interface StretchedTextProps {
@@ -22,17 +22,38 @@ interface StretchedTextByLetterSizeProps {
     text: string;
     gap?: number;
     elementId?: string;
-    thinnerLettersIndex?: number[]
+    thinnerLettersIndex?: number[];
 }
 
 export function StretchedTextByLetterSize({className, text, gap = 1.5, elementId, thinnerLettersIndex}: StretchedTextByLetterSizeProps){
+    // const textRef = useRef<HTMLSpanElement>(null);
+    // const lettersRef = useRef<(HTMLSpanElement | null)[]>([]);
+    // const [textWidth, setTextWidth] = useState('0px')
+    //
+    // useEffect(() => {
+    //     if (!textRef.current || !lettersRef.current) return;
+    //
+    //     const letterWidths: number[] = [];
+    //     lettersRef.current.forEach((el) => {
+    //         if (!el || !el.parentElement) return;
+    //         const marginLeft = parseFloat(el.style.marginLeft) * parseFloat(getComputedStyle(el).fontSize);
+    //         const width = el.clientWidth + marginLeft;
+    //
+    //         letterWidths.push(width);
+    //     })
+    //
+    //     setTextWidth(Math.max(...letterWidths) + 'px');
+    //     // setTextHeight(lettersRef.current[0]?.clientHeight + 'px');
+    // }, [text, gap, thinnerLettersIndex]);
 
     return(
-        <span className={'relative block ' + className} style={{ width: `${text.length * gap - (text.length / 10)}em` }}>
+            <span className={'relative block ' + className}>
             {Array.from(text).map((letter, index) => {
-                return <span style={{marginLeft: `${index * gap - (thinnerLettersIndex?.includes(index) ? .1 : 0)}em`}} className={'absolute left-0 w-[1em]'} id={elementId} key={index}>{letter}</span>
+                return <span
+                    style={{marginLeft: `${index * gap - (thinnerLettersIndex?.includes(index) ? .1 : 0)}em`}}
+                    className={'absolute left-0 w-[1em]'} id={elementId} key={index}>{letter}</span>
             })}
-            <span className={'invisible'}>{text}</span>
+                <span className={'invisible'}>{text}</span>
         </span>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import {StretchedTextByLetterSize} from "@/app/components/Text";
 import {useEffect, useRef} from "react";
 import anime from "animejs/lib/anime.es";
 
@@ -20,26 +19,29 @@ export default function Home() {
 
   return (
     <>
-      <main className={'h-screen bg-primary-gray bg-gridPattern bg-[length:200px] bg-[left_35px_top_20px] '}>
-          <div className={'absolute w-full h-full bg-[linear-gradient(140deg,_hsla(193,77%,55%,3%)_0%,_hsla(193,77%,55%,0%)_100%)]'}></div>
+      <main className={'h-screen bg-alt-gray-primary bg-gridPattern bg-[length:200px] bg-[left_35px_top_20px]'}>
           <div className={'relative z-0'}>
-              <div className={'uppercase text-white font-bold ms-24'}>
-                  <span className={'text-4xl text-[#808080]'}>Student & Developer</span>
-                  <h1 className={'relative text-[200px]'}>
-                      {/*<StretchedText>qwe asd</StretchedText>*/}
-                      {/*<StretchedText><span className={'text-aero-500'}>J</span> a k u b</StretchedText>*/}
-                      {/*<StretchedText><span className={'text-aero-500'}>S</span> o k o l</StretchedText>*/}
-                      {/*<span className={'text-aero-500'}>J</span>akub<br/>*/}
-                      {/*<span className={'text-aero-500'}>S</span>okol*/}
-                      <div className={'inline-block relative w-auto '}>
-                          <StretchedTextByLetterSize className={'select-none'} text={'Jakub'} thinnerLettersIndex={[1]}/>
-                          <div className={'absolute w-full h-full top-0 -left-8 bg-aero-500 origin-left'} ref={el => { if (el) titleRefs.current[0] = el; }}></div>
-                      </div>
-                      <div className={'inline-block relative w-auto '}>
-                          <StretchedTextByLetterSize className={'select-none'} text={'Sokol'} thinnerLettersIndex={[1]}/>
-                          <div className={'absolute w-full h-full top-0 -left-8 bg-aero-500 origin-left'} ref={el => { if (el) titleRefs.current[1] = el; }}></div>
-                      </div>
-                  </h1>
+              <div className={'uppercase text-white font-extrabold ms-24 inline-flex flex-col mt-96'}>
+                  <div className={'relative inline-block text-[300px]'}>
+                      <span className={'ms-[.2em] -mb-20 block'}>
+                          <h2 className={'text-4xl text-alt-gray-500 ms-[.2em]'}>Student & Developer</h2>
+                      </span>
+                      <h1 className={'relative grid grid-cols-5 gap-x-24 pe-10'}>
+                          {
+                              Array.from('JakubSokol').map((letter, index) => {
+                                  return <span key={index}
+                                               className={index == 0 || index == 5 ? 'ms-[.2em]' : undefined}>{letter}</span>
+                              })
+                          }
+                          <div className="absolute top-0 left-0 w-full h-full grid grid-cols-5">
+                              {
+                                  Array(2).fill(0).map((_, index) => (
+                                      <div key={index} className="col-span-full bg-aero-500 h-full origin-left" ref={el => { if (el) titleRefs.current[index] = el; }}></div>
+                                  ))
+                              }
+                          </div>
+                      </h1>
+                  </div>
               </div>
           </div>
       </main>
