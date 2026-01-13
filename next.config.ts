13f36dd8868653f1c93e4ab56596f92757381ca1
Jakub Sokol <sokoljakub14@gmail.com>
2026-01-13 16:49:24 +0100

    chore: remove deprecated experimental turbo pack option

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    experimental: {
-        turbo: {
-            rules: {
-                "*.scss": {
-                    loaders: ["sass-loader"],
-                    as: "*.css",
-                },
-            }
-        }
-    }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
